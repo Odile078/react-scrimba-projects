3a1cf2f7aa4f6722365451600d4280c00ccb7497
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-03-05 06:14:39 +0200

    feat: add cardlist  component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import CardsList from "./components/CardsList";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="max-w-[550px] mx-auto">
+    <div className="max-w-[550px] mx-auto pb-6">
       <Navbar />
       <Hero />
+      <CardsList />
     </div>
   );
 }
