d48a2c53acb8bd8b67e674f7ae270331ff40294d
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-03-13 17:57:45 +0200

    feat: add confetti on game win

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,31 @@
 import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 
 function MainContent() {
   const [diceList, setDiceList] = useState([]);
-  const [pickedDice, setPickedDice] = useState(null);
+  const [allDicePicked, setAllDicePicked] = useState(false);
+  const [width, setWindowWidth] = useState(window?.innerWidth);
+  const [height, setWindowHeight] = useState(window?.innerHeight);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(Window?.innerHeight);
+    };
 
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const checkAllDicePicked = (dices) => {
+    let remainingDiceList = dices.filter((dice) => dice?.isPicked !== true);
+    if (remainingDiceList?.length === 0) {
+      setAllDicePicked(true);
+      console.log("release confetti");
+    } else return setAllDicePicked(false);
+  };
   const getRandomDiceFullList = (newDiceList) => {
     const newDice = newDiceList || [];
     for (let i = 0; i < 10; i++) {
@@ -15,24 +37,19 @@ function MainContent() {
     setDiceList(newDice);
   };
   const getRandomDiceRemainingList = () => {
-    let remainingDiceList = diceList.filter((dice) => dice?.isPicked !== true);
-    if (remainingDiceList?.length === 0) {
-      getRandomDiceFullList();
-    } else {
-      let dices = diceList.slice();
-      dices.map((dice, i) =>
-        dice?.isPicked === false
-          ? (dices[i] = {
-              diceValue: Math.ceil(Math.random() * 6),
-              isPicked: false,
-            })
-          : dice
-      );
-      setDiceList(dices);
-    }
+    let dices = diceList.slice();
+    dices.map((dice, i) =>
+      dice?.isPicked === false
+        ? (dices[i] = {
+            diceValue: Math.ceil(Math.random() * 6),
+            isPicked: false,
+          })
+        : dice
+    );
+    setDiceList(dices);
   };
   const getRandomDiceList = () => {
-    pickedDice ? getRandomDiceRemainingList() : getRandomDiceFullList();
+    allDicePicked ? getRandomDiceFullList() : getRandomDiceRemainingList();
   };
   const pickDice = (diceIndex) => {
     let dices = diceList.slice();
@@ -43,40 +60,47 @@ function MainContent() {
       dices[diceIndex] = { ...dices[diceIndex], isPicked: false };
       setDiceList(dices);
     }
-    setPickedDice(diceList[diceIndex]);
+    checkAllDicePicked(dices);
   };
+
   useEffect(() => {
     getRandomDiceFullList();
   }, []);
+
   return (
-    <div className="flex justify-center items-center flex-col gap-5">
-      <h1 className="text-gray-800 font-bold text-2xl">Tenzies</h1>
-      <p className="text-gray-600 text-sm">
-        Roll until all dice are the same. Click each die to freeze it at its
-        current value between rolls.
-      </p>
-      <div className="grid grid-cols-2 sm:grid-cols-5 gap-4">
-        {diceList?.length > 0
-          ? diceList?.map((item, index) => (
-              <button
-                key={index}
-                onClick={() => pickDice(index)}
-                className={`${
-                  item?.isPicked === true ? "bg-green-400" : "bg-white"
-                } rounded-md text-center p-5 py-3 text-lg font-bold text-gray-800 drop-shadow-md`}
-              >
-                {item?.diceValue}
-              </button>
-            ))
-          : null}
+    <>
+      {allDicePicked ? (
+        <Confetti width={width} height={height}></Confetti>
+      ) : null}
+      <div className=" relative flex justify-center items-center flex-col gap-5">
+        <h1 className="text-gray-800 font-bold text-2xl">Tenzies</h1>
+        <p className="text-gray-600 text-sm">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+        <div className="grid grid-cols-2 sm:grid-cols-5 gap-4">
+          {diceList?.length > 0
+            ? diceList?.map((item, index) => (
+                <button
+                  key={index}
+                  onClick={() => pickDice(index)}
+                  className={`${
+                    item?.isPicked === true ? "bg-green-400" : "bg-white"
+                  } rounded-md text-center p-5 py-3 text-lg font-bold text-gray-800 drop-shadow-md`}
+                >
+                  {item?.diceValue}
+                </button>
+              ))
+            : null}
+        </div>
+        <button
+          onClick={getRandomDiceList}
+          className="rounded-md w-fit bg-sky-500 py-1 px-5 text-base text-white"
+        >
+          {allDicePicked ? "New game" : "Roll"}
+        </button>
       </div>
-      <button
-        onClick={getRandomDiceList}
-        className="rounded-md w-fit bg-sky-500 py-1 px-5 text-base text-white"
-      >
-        Roll
-      </button>
-    </div>
+    </>
   );
 }
 
