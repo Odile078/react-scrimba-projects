adcd44118c6c8625df2cfea5c94b0d512878f7d7
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-03-14 11:55:48 +0200

    feat: set pu pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandBlue: {
+          lighter: "F5F7FB",
+          light: "D6DBF5",
+          DEFAULT: "4D5B9E",
+          darker: "293264",
+        },
+      },
+    },
   },
   plugins: [],
 };
